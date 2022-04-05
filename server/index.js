f8769d4287d8542d05de70a6259aa93a05e7c68f
Tjaša Flis <tflis11@gmail.com>
2022-04-05 08:36:03 +0200

    setup cors in node

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 // Instantiate app
 const app = express();
+
+// CORS setup
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // Start app on port 3001
 app.listen(3001, () => {
