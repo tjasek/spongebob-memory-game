97603a6916a10ddcc6a491be1c1c1c28287f6530
Tjaša Flis <tflis11@gmail.com>
2022-04-05 08:46:15 +0200

    create the entry page with 2 buttons - single player and multiplayer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-import logo from "./spongebob-logo.png";
+import { useState } from "react";
 import "./App.css";
+import logo from "./spongebob-logo.png";
 
 function App() {
+  const [mode, setMode] = useState("");
+
+  const selectedSinglePlayerMode = () => {
+    setMode("singlePlayer");
+  };
+
+  const selectedMultiPlayerMode = () => {
+    setMode("multiPlayer");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">SpongeBob Memory Game</h1>
-        <div className="App-mode-buttons">
-          <button>Single Player</button>
-          <button>Multi Player</button>
-        </div>
-      </header>
+      {mode === "" && (
+        <>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">SpongeBob Memory Game</h1>
+          <div className="App-mode-buttons">
+            <button onClick={selectedSinglePlayerMode}>Single Player</button>
+            <button onClick={selectedMultiPlayerMode}>Multi Player</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
